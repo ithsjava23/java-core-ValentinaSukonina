7e2b531771acc8e960bf4b5e88659d544d925a33
ValentinaSukonina <v.trana@icloud.com>
2023-10-19 20:13:11 +0200

    final category restored

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -7,6 +7,8 @@ public class Category {
 
     private final String name;
 
+    private static final HashMap<String, Category> categories = new HashMap<>();
+
     private Category(String name) {
         if (name == null)
             throw new IllegalArgumentException("Category name can't be null");
@@ -18,8 +20,19 @@ public class Category {
     }
 
     public static Category of(String name) {
+        if (name == null)
+            throw new IllegalArgumentException("Category name can't be null");
+
+        if (!categories.containsKey(name))
+            categories.put(name, new Category(name));
+
+        if (categories.containsKey(name))
+            return categories.get(name);
+
         Category category = new Category(name);
+        categories.put(name, category);
         return category;
+
     }
 
     // * Override the equals method to compare two Category instances by their names
@@ -29,6 +42,7 @@ public class Category {
         if (o == null || getClass() != o.getClass()) return false;
         Category category = (Category) o;
         return Objects.equals(name, category.name);
+
     }
 
     @Override

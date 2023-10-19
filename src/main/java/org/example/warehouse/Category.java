9de02d74dc0cc4b7c23b609648ad8aac77d4ce0b
ValentinaSukonina <v.trana@icloud.com>
2023-10-19 20:10:13 +0200

    final category changed to remove code for inactive test

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -7,8 +7,6 @@ public class Category {
 
     private final String name;
 
-    private static final HashMap<String, Category> categories = new HashMap<>();
-
     private Category(String name) {
         if (name == null)
             throw new IllegalArgumentException("Category name can't be null");
@@ -20,17 +18,7 @@ public class Category {
     }
 
     public static Category of(String name) {
-        if (name == null)
-            throw new IllegalArgumentException("Category name can't be null");
-
-        if (!categories.containsKey(name))
-            categories.put(name, new Category(name));
-
-        if (categories.containsKey(name))
-            return categories.get(name);
-
         Category category = new Category(name);
-        categories.put(name, category);
         return category;
     }
 
@@ -41,7 +29,6 @@ public class Category {
         if (o == null || getClass() != o.getClass()) return false;
         Category category = (Category) o;
         return Objects.equals(name, category.name);
-
     }
 
     @Override
